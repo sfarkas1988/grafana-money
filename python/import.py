4e27264823920bf278dd29f8f5c9deaa4fb32357
sfarkas <sandor.farkas_ext@momox.biz>
2019-05-20 21:14:59 +0200

    iterate through a list of csv files

--- python/import.py
+++ python/import.py
@@ -1,4 +1,22 @@
 import csv
-rows = csv.reader(open('Girokonto.csv', newline=''), delimiter=';')
-for row in rows:
-    print(', '.join(row))
+import os
+
+from os import listdir
+from os.path import isfile, join
+
+csv_path = "./csv"
+files = [f for f in listdir(csv_path) if isfile(join(csv_path, f))]
+
+
+for f in files:
+    file_path = csv_path +"/"+f
+    file_extension = os.path.splitext(file_path)
+    if file_extension[1] != ".csv":
+        print(file_extension[1])
+        continue
+
+    print(file_path)
+    rows = csv.reader(open(file_path, newline=''), delimiter=';')
+    for row in rows:
+        print(', '.join(row))
+

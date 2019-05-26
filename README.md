b266aab3ac55c4e24a1f74d832d9a8c8f5dc82e9
Sandor Farkas <farkas.berlin@gmail.com>
2019-05-26 16:42:13 +0200

    wip

---
+++ README.md
@@ -0,0 +1,14 @@
+# Installation
+
+```
+git clone git@github.com:sfarkas1988/grafana-money.git
+cp .env_example .env
+nano .env
+docker-compose up -d --build
+```
+
+# usage
+
+```
+http://localhost:8081
+```
--- python/import.py
+++ python/import.py
@@ -1,4 +1,18 @@
 import csv
-rows = csv.reader(open('Girokonto.csv', newline=''), delimiter=';')
-for row in rows:
-    print(', '.join(row))
+from os import walk
+import db
+
+connection = db.connect()
+#db.insert_sales()
+#
+# path = './csv'
+# for (dirpath, dirnames, filenames) in walk(path):
+#     for filename in filenames:
+#         if filename.endswith('.csv'):
+#             rows = csv.reader(open(path+'/'+filename, newline=''), delimiter=';')
+#             for row in rows:
+#                 print(row)
+#             break
+
+
+#Datum;Wertstellung;Kategorie;Name;Verwendungszweck;Konto;Bank;Betrag;Währung
